37f857ec63455fb5ed37d31d0867e0181c06dda7
Omar Razzouki Rodriguez <Omar.razzouki.03@gmail.com>
2022-09-29 11:42:16 +0200

    Ejercicio 2 php

---
+++ sprint1apache/www/site3/example2.php
@@ -0,0 +1,7 @@
+<html>
+	<body>
+		<h1>Página de bienvenidas</h2>
+		<?php function dar_bienvenida($nombre) { echo "¡Bienvenido/a " . $nombre . "!"; }
+		dar_bienvenida("Mario Casas");?>
+	</body>
+</html>
