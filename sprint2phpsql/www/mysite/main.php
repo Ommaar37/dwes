c81297922babc7b6f280186328204b194db422dc
Ommaar37 <113436064+Ommaar37@users.noreply.github.com>
2022-10-17 13:34:17 +0200

    Delete main.php

--- sprint2phpsql/www/mysite/main.php
+++
@@ -1,8 +0,0 @@
-<html>
-	<body>
-			<?php
-				$db = mysqli_connect('localhost','root','1234','mysitedb') or die('Fail'.mysqli_connect_error());
-			?>
-			<h1>Conexión establexida</h1>
-	</body>
-</html>
