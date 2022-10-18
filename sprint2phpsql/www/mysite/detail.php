c3a7d37542119eada2284ff4ecad810cd33e94dc
Ommaar37 <113436064+Ommaar37@users.noreply.github.com>
2022-10-18 13:29:04 +0200

    Update detail.php

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -4,11 +4,11 @@
 <html>
 	<body>
 		<?php
-			if (!isset($_GET['cancion_id'])){
+			if (!isset($_GET['pelicula_id'])){
 				die('No se ha especificado una pelicula');
 			}
-			$cancion_id = $_GET['cancion_id'];
-			$query = 'SELECT * FROM tCanciones WHERE id='.$cancion_id;
+			$pelicula_id = $_GET['pelicula_id'];
+			$query = 'SELECT * FROM tPeliculas WHERE id='.$pelicula_id;
 			$result = mysqli_query($db, $query) or die ('Query error');
 			$only_row = mysqli_fetch_array($result);
 			echo '<h1>'.$only_row['1'].'</h1>';
@@ -20,7 +20,7 @@
 		<h3>Comentarios</h3>
 		<ul>
 			<?php
-				$query2 = 'SELECT * FROM tComentarios WHERE cancion_id='.$cancion_id;
+				$query2 = 'SELECT * FROM tComentarios WHERE pelicula_id='.$pelicula_id;
 				$result2 = mysqli_query($db, $query2) or die('Query error2');
 				while ($row = mysqli_fetch_array($result2)){
 					$fecha = $row['fecha'];
@@ -33,5 +33,11 @@
 				mysqli_close($db);
 			?>
 		</ul>
+		<p>Deja un nuevo comentario:</p>
+		<form action="/comment.php" method="post">
+			<textarea rows="4" cols="50" name="new_comment"></textarea><br>
+			<input type="hidden" name="pelicula_id" value="<?php echo $pelicula_id; ?>">
+			<input type="submit" value="Comentar">
+		</form>
 	</body>
 </html>
