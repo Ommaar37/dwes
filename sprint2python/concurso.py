953ece2d05a5d0d17aed99fcf81e4e92cbb3bf8c
Ommaar37 <113436064+Ommaar37@users.noreply.github.com>
2022-10-18 14:22:15 +0200

    Ejercicio 2, actualización de concurso.py

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -1,13 +1,41 @@
 print("Concurso!!")
+print("Se le realizaran 3 preguntas que cada una correcta suma 10 puntos y cada erronea resta 5.")
+print("Al finalizar las 3 se le mostrará el resultado final.")
+
+print("Primera pregunta...")
 print("¿Cúal es el jugador con más balones de oro de la historia?")
 print("A) Messi.")
 print("B) Quiles.")
 print ("C) Danilo Pereira.")
 respuesta=input().upper()
-if respuesta != "B" and respuesta != "A" and respuesta != "C":
-    print("Respuesta fuera de rango, concurso perdido.")
-elif respuesta == "B":
-    print("¡CORRECTO!")
+puntuation = 0
+if respuesta == "B":
+   puntuation= puntuation+10
 else:
-    print("Respuesta incorrecta.")
+    puntuation= puntuation-5
+
+print("Segunda pregunta...")
+print("¿Cúal es el mejor día del mes?")
+print("A) El 18.")
+print("B) El 23.")
+print ("C) El 30.")
+respuesta=input().upper()
+if respuesta == "A":
+   puntuation= puntuation+10
+else:
+    puntuation= puntuation-5
+
+print("Tercera pregunta...")
+print("¿Cúal es el mejor día de entre semana?")
+print("A) Lunes.")
+print("B) Miercoles.")
+print ("C) Jueves.")
+respuesta=input().upper()
+if respuesta == "C":
+   puntuation= puntuation +10
+else:
+    puntuation= puntuation-5
+print("Puntuación total:")
+print("Su puntuación total es de...")
+print(puntuation)
 
