f5a045f30f67ae8de1d02f0d04c69c9600e0c307
Ommaar37 <113436064+Ommaar37@users.noreply.github.com>
2022-10-18 13:10:28 +0200

    Update detail.php

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -1,46 +1,37 @@
 <?php
-$db = mysqli_connect('localhost', 'root', '1234', 'tCanciones') or die('Fail');
+	$db = mysqli_connect('localhost','root','1234','mysitedb')or die('Fail');
 ?>
 <html>
-<body>
-<?php
-if (!isset($_GET['cancion_id'])) {
-die('No se ha especificado una canción');
-}
-$cancion_id = $_GET['cancion_id'];
-$query = 'SELECT * FROM tCanciones WHERE id='.$cancion_id;
-$result = mysqli_query($db, $query) or die('Query error');
-$only_row = mysqli_fetch_array($result);
-echo '<h1>'.$only_row['titulo'].'</h1>';
-echo '<h2>'.$only_row['año'].'</h2>';
-?>
-<h3>Comentarios:</h3>
-<ul>
-<?php
-$query = 'SELECT * FROM tCanciones WHERE id=$cancion_id';
-			
-			$result = mysqli_query($db, $query) or die('Query error');
-			//Recorrer el resultado
-			while ($row = mysqli_fetch_array($result)){
-			echo $row[0];
-			echo '<br>';
-			echo  $row['1'];
-			echo '<br>';
-			echo '<img src="'.$row['2'];
-			echo '">';
-			echo '<br>';
-			echo $row['3'];
-			echo '<br>';
-			echo $row['4'];
-			echo '<br>';
+	<body>
+		<?php
+			if (!isset($_GET['cancion_id'])){
+				die('No se ha especificado una pelicula');
 			}
-$query2 = 'SELECT * FROM tComentarios WHERE cancion_id='.$cancion_id;
-$result2 = mysqli_query($db, $query2) or die('Query error');
-while ($row = mysqli_fetch_array($result2)) {
-echo '<li>'.$row['comentario'].'</li>';
-}
-mysqli_close($db);
-?>
-</ul>
-</body>
+			$cancion_id = $_GET['cancion_id'];
+			$query = 'SELECT * FROM tCanciones WHERE id='.$cancion_id;
+			$result = mysqli_query($db, $query) or die ('Query error');
+			$only_row = mysqli_fetch_array($result);
+			echo '<h1>'.$only_row['1'].'</h1>';
+			echo '<img src="'.$only_row['2'];
+			echo '">';
+			echo '<h2>'.$only_row['3'].'</h2>';
+			echo '<h2>'.$only_row['4'].'</h2>';
+		?>
+		<h3>Comentarios</h3>
+		<ul>
+			<?php
+				$query2 = 'SELECT * FROM tComentarios WHERE pelicula_id='.$pelicula_id;
+				$result2 = mysqli_query($db, $query2) or die('Query error2');
+				while ($row = mysqli_fetch_array($result2)){
+					$fecha = $row['fecha'];
+					if (is_null($fecha)) {
+						echo '<li>'.$row['1'].' (Sin fecha registrada)</li>';
+					}else {
+						echo '<li>'.$row['1'].' ('.$row['fecha'].')</li>';
+					}
+				}
+				mysqli_close($db);
+			?>
+		</ul>
+	</body>
 </html>
