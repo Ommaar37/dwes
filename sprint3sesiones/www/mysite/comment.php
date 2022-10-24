78bb3be3f7dd22b58af51f5ff6715fbc61d2d1df
Ommaar37 <113436064+Ommaar37@users.noreply.github.com>
2022-10-24 13:18:30 +0200

    cambio en comment.php ejercicio 4

--- sprint3sesiones/www/mysite/comment.php
+++ sprint3sesiones/www/mysite/comment.php
@@ -1,24 +1,24 @@
 <?php
-	$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
 <html>
-	<body>
-	<?php
-		$cancion_id  = $_POST['cancion_id'];
-		$comentario = $_POST['new_comment'];
-		$fecha = date('Y-m-d');
-
-		$query = "INSERT INTO tComentarios (comentarios, usuario_id, cancion_id, fecha) VALUES ('".$comentario."', NULL, ".$cancion_id.",'".$fecha."');";
-echo $query;
-	 	mysqli_query($db, $query) or die('Error');
-
-		echo "<p> Nuevo comentario ";
-		echo mysqli_insert_id($db);
-		echo " añadido </p>";
-
-		echo "<a href='/detail.php?cancion_id=".$cancion_id;
-		echo "'>Volver</a>";
-		mysqli_close($db);
-	?>
-	</body>
+<body>
+<?php
+session_start();
+$user_id_a_insertar = 'NULL';
+if (!empty($_SESSION['user_id'])) {
+$user_id_a_insertar = $_SESSION['user_id'];
+}
+$cancion_id = $_POST['cancion_id'];
+$comentario = $_POST['new_comment'];
+$query = "INSERT INTO tComentarios(comentario, cancion_id, usuario_id)
+VALUES ('".$comentario."',".$cancion_id.",".$user_id_a_insertar.")";
+mysqli_query($db, $query) or die('Error');
+echo "<p>Nuevo comentario ";
+echo mysqli_insert_id($db);
+echo " añadido</p>";
+echo "<a href='/detail.php?cancion_id=".$cancion_id."'>Volver</a>";
+mysqli_close($db);
+?>
+</body>
 </html>
